432209ccddfc4b19d4682ffde16502b68c67d8a2
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2023-10-09 15:56:12 -0500

    fixed missed steps

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    $("#minimize").on("click", function(){
-        $("p").css("font-size", "7px")
-    })
-})
+    $("#minimize").on("click", function () {
+        $(".footer").css("font-size", "7px");
+        $(".footnote").css("font-size", "10px");
+    });
+ })
